1260d4920b6319c6142fa19c6899bcb72499b1e5
kwhitejr <kwhitejr.edu@gmail.com>
2016-01-09 19:38:21 -1000

    step 8.iv

--- todo_app.sql
+++ todo_app.sql
@@ -5,3 +5,23 @@ DROP USER michael;
 CREATE USER michael WITH ENCRYPTED PASSWORD 'stonebreaker';
 
 DROP DATABASE IF EXISTS todo_app;
+
+CREATE DATABASE todo_app;
+
+\c todo_app michael;
+
+CREATE TABLE tasks (
+id int primary key not null,
+title char(255) not null,
+description text,
+created_at timestamp without time zone not null default now(),
+updated_at timestamp without time zone,
+completed boolean not null default false
+);
+
+ALTER TABLE tasks DROP completed;
+
+ALTER TABLE tasks ADD completed_at timestamp without time zone default null;
+
+ALTER TABLE tasks ALTER updated_at SET not null;
+
